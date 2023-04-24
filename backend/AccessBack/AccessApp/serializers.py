211a5e492958e1d41fe562f6fbe81a5fb1d1a843
Ikotun Collins <danlogan2003@gmail.com>
2023-04-24 19:38:10 +0100

    .....serializers

---
+++ backend/AccessBack/AccessApp/serializers.py
@@ -0,0 +1,16 @@
+from .models import App_user, Task
+from rest_framework import serializers
+
+#serializer for Appusers
+class User_serializer(serializers.ModelSerializer):
+    class Meta :
+        model : 'App_user'
+        fields : ('user_name', 'password')
+
+
+#serializer for Task
+class Task_serializer(serializers.ModelSerializer):
+    class Meta :
+        model : 'Task'
+        fields : ('description', 'details', 'reminder')
+        
--- backend/AccessBack/AccessApp/views.py
+++ backend/AccessBack/AccessApp/views.py
@@ -2,5 +2,20 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status 
+from .models import Task, App_user
+from .serializers import Task_serializer, User_serializer
+
+
+
 
 # Create your views here.
+class Login_view(APIView):
+    #FOR GET REQUEST 
+    def get(request):
+        pass
+
+    #FOR POST REQUEST
+    def post(request):
+        pass
+
+    
