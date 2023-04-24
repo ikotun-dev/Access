fb09fe07df447dfea7bc9c861408fe1fdb856cd1
Ikotun Collins <danlogan2003@gmail.com>
2023-04-24 19:29:23 +0100

    >

--- backend/AccessBack/AccessApp/models.py
+++ backend/AccessBack/AccessApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+##the app user 
+class App_user(models.Model):
+    user_name = models.CharField(max_length=300)
+    password = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f"self.user_name"
+    
+##the TASK 
+class Task(models.Model):
+    description = models.CharField(max_length=400)
+    detail = models.CharField(max_length=400)
+    reminder = models.BooleanField(default=False)
+
+    
+
+    
