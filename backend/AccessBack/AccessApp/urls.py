7775baeeae7f0d936b29297ca7183c1fb63ec4c7
Ikotun Collins <danlogan2003@gmail.com>
2023-04-27 10:03:08 +0100

    add task ft

--- backend/AccessBack/AccessApp/urls.py
+++ backend/AccessBack/AccessApp/urls.py
@@ -6,5 +6,7 @@ from . import views
 urlpatterns = [
     path('login/', views.Login_view.as_view()),
     path('signup/', views.Sign_up.as_view()),
+    path('add-task/', views.AddTask.as_view()),
+    
 
 ]
--- backend/AccessBack/AccessApp/views.py
+++ backend/AccessBack/AccessApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status 
@@ -6,32 +7,44 @@ from .models import Task, App_user
 from .serializers import Task_serializer, User_serializer
 from rest_framework_simplejwt.tokens import RefreshToken
 
-
 a_user = App_user.objects.all()
 
-# Create your views here.
 class Login_view(APIView):
     #FOR GET REQUEST 
     def post(self, request, format=None):
+
         serializer = User_serializer(data=request.data)
+
         if serializer.is_valid():
-            try : 
-                i_username = serializer.validated_data['user_name']
-                i_password = serializer.validated_data['password']
+            i_username = serializer.validated_data['user_name']
+            i_password = serializer.validated_data['password']
 
-                users = App_user.objects.get(user_name=i_username, password=i_password)
-                if users : 
-                    return Response({'success' : 'Login worked'}, status=status.HTTP_200_OK)
-                else :
-                    return Response({'Failure' : 'didnt work'}, status=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION)
-
-
-            except Exception:
-                return Response({'Failure' : 'login failed'}, status=status.HTTP_401_UNAUTHORIZED)
-    
-        else :
+            users = App_user.objects.get(user_name=i_username, password=i_password)
+            if users:
+                refresh = RefreshToken.for_user(users)
+                access_token = refresh.access_token
+                response = {
+                    'success' : 'Login Worked',
+                    'refresh' : str(refresh),
+                    'access' : str(access_token)
+                }
+                return Response(response, status=status.HTTP_200_OK)
+            else:
+                return Response({'Failure': 'didnt work'}, status=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION)
+        else:
             return Response({'Invalid data inputed'}, status=status.HTTP_400_BAD_REQUEST)
+
+class AddTask(APIView):
+    #posting - adding a new task
+    def post(self, request, format=None):
+        serializer = Task_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
             
+
+
+
+
 class Sign_up(APIView):
     def post(self, request):
         serializer = User_serializer(data=request.data)
